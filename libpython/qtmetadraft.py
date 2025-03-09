62df20be2364a068b7e47888bf5d4a8c20d2b67f
bgoli <brettolivier@gmail.com>
2025-03-09 17:25:03 +0100

    fixed annotation colur selection schemes

--- libpython/qtmetadraft.py
+++ libpython/qtmetadraft.py
@@ -445,159 +445,6 @@ class StreamToLogger(object):
 DEBUG_MODE = True
 DEL_BLAST_TMP = False
 
-
-_RGB_COLOURS_136_ = (
-    (128,0,0),
-    #(139,0,0),
-    (165,42,42),
-    (178,34,34),
-    (220,20,60),
-    (255,0,0),
-    (255,99,71),
-    (255,127,80),
-    (205,92,92),
-    (240,128,128),
-    (233,150,122),
-    (250,128,114),
-    (255,160,122),
-    (255,69,0),
-    (255,140,0),
-    (255,165,0),
-    (255,215,0),
-    (184,134,11),
-    (218,165,32),
-    (238,232,170),
-    (189,183,107),
-    (240,230,140),
-    (128,128,0),
-    (255,255,0),
-    (154,205,50),
-    (85,107,47),
-    (107,142,35),
-    (124,252,0),
-    (127,255,0),
-    (173,255,47),
-    (0,100,0),
-    (0,128,0),
-    (34,139,34),
-    (0,255,0),
-    (50,205,50),
-    (144,238,144),
-    (152,251,152),
-    (143,188,143),
-    (0,250,154),
-    (0,255,127),
-    (46,139,87),
-    (102,205,170),
-    (60,179,113),
-    (32,178,170),
-    (47,79,79),
-    (0,128,128),
-    #(0,139,139),
-    (0,255,255),
-    (0,255,255),
-    (224,255,255),
-    (0,206,209),
-    (64,224,208),
-    (72,209,204),
-    (175,238,238),
-    (127,255,212),
-    (176,224,230),
-    (95,158,160),
-    (70,130,180),
-    (100,149,237),
-    (0,191,255),
-    (30,144,255),
-    (173,216,230),
-    (135,206,235),
-    (135,206,250),
-    (25,25,112),
-    (0,0,128),
-    #(0,0,139),
-    (0,0,205),
-    (0,0,255),
-    (65,105,225),
-    (138,43,226),
-    (75,0,130),
-    (72,61,139),
-    (106,90,205),
-    (123,104,238),
-    (147,112,219),
-    (139,0,139),
-    (148,0,211),
-    (153,50,204),
-    (186,85,211),
-    (128,0,128),
-    (216,191,216),
-    (221,160,221),
-    (238,130,238),
-    (255,0,255),
-    (218,112,214),
-    (199,21,133),
-    (219,112,147),
-    (255,20,147),
-    (255,105,180),
-    (255,182,193),
-    (255,192,203),
-    (250,235,215),
-    (245,245,220),
-    (255,228,196),
-    (255,235,205),
-    (245,222,179),
-    (255,248,220),
-    (255,250,205),
-    (250,250,210),
-    (255,255,224),
-    (139,69,19),
-    (160,82,45),
-    (210,105,30),
-    (205,133,63),
-    (244,164,96),
-    (222,184,135),
-    (210,180,140),
-    (188,143,143),
-    (255,228,181),
-    (255,222,173),
-    (255,218,185),
-    (255,228,225),
-    (255,240,245),
-    (250,240,230),
-    (253,245,230),
-    (255,239,213),
-    (255,245,238),
-    (245,255,250),
-    (112,128,144),
-    (119,136,153),
-    (176,196,222),
-    (230,230,250),
-    (255,250,240),
-    (240,248,255),
-    (248,248,255),
-    (240,255,240),
-    (255,255,240),
-    (240,255,255),
-    (255,250,250),
-    (105,105,105),
-    (128,128,128),
-    (169,169,169),
-    (192,192,192),
-    (211,211,211),
-    (220,220,220),
-)
-
-_RGB_COLOURS_10_ = (
-    (255, 255, 204),
-    (204, 255, 204),
-    (204, 255, 255),
-    (204, 204, 255),
-    (255, 204, 204),
-    (204, 255, 153),
-    (153, 255, 204),
-    (102, 204, 255),
-    (204, 153, 255),
-    (255, 153, 204),
-)
-
 class MetaDraftGUI(QWidget):
     """
     The MetaDraftGUI class is responsible for the graphical user interface of the MetaDraft application.
@@ -802,7 +649,8 @@ class MetaDraftGUI(QWidget):
     grid = None
     status_bar = None
     _colourCycler_ = None
-    _colours_ = _RGB_COLOURS_136_ # needs testing
+    _colour_pallet_ = rgb_tables.RGB_DARK_COLOUR_TABLE
+    _colour_pallet_name_ =  'RGB_DARK_COLOUR_TABLE'
     _loading_ = None
     _active_tab_ = 0
     _last_tab_ = 0
@@ -3768,7 +3616,7 @@ the template library submodule has been initialised (see readme.md) and correctl
         self.widget_busy.setLabelText("Loading results")
         self.widget_busy.show()
         self.widgetBusyUpdate(10)
-        self._colourCycler_ = itertools.cycle(self._colours_)
+        self._colourCycler_ = itertools.cycle(self._colour_pallet_)
         self.widgetBusyUpdate(50)
         self.getMetaProteomeData(self.result_file)
         self.widgetBusyUpdate(70)
@@ -4772,7 +4620,14 @@ the template library submodule has been initialised (see readme.md) and correctl
                             items[c_].setFlags(items[c_].flags() & ~QtCore.Qt.ItemFlag.ItemIsEditable)
                         if grp_colour != None:
                             items[c_].setBackground(QColor(*grp_colour))
+                            print(grp_colour)
+                            # this needs to made more sophisticated but until we have darkmode detection we force dark background and white text ... hopefully.
+                            if self._colour_pallet_name_ == 'RGB_DARK_COLOUR_TABLE':
+                                text_colour =  (255, 255, 255)
+                                items[c_].setForeground(QColor(*text_colour))
+                                
                         self.table_gene.setItem(row, c_, items[c_])
+                        
 
                     row += 1
             else:
@@ -5364,8 +5219,12 @@ the template library submodule has been initialised (see readme.md) and correctl
         if reac == '':
             return 'No matching reaction'
 
-        bkg_clr_sub = '#0000FF' #'#CCFFCC'
-        bkg_clr_prd = '#4B0082' #'#FFCCCC'
+        # we are going to hack this to use our rgb_tables 
+        #bkg_clr_sub = '#0000FF' #'#CCFFCC'
+        #bkg_clr_prd = '#4B0082' #'#FFCCCC'
+        bkg_clr_sub = rgb2hex(self._colour_pallet_[0])
+        bkg_clr_prd = rgb2hex(self._colour_pallet_[-20])
+
 
         r_html = '<html><head>'
 
@@ -5488,7 +5347,9 @@ the template library submodule has been initialised (see readme.md) and correctl
     def buildHtmlStringsMetab(self, metab):
         if metab == '':
             return 'No matching metabolite'
-        bkg_clr = '#008000'
+        # make more sophisticated
+        #bkg_clr = '#008000'
+        bkg_clr = rgb2hex(self._colour_pallet_[-15])
         
         S = self.selected_metabolites[metab]
         r_html = '<html><body>'
@@ -6648,6 +6509,8 @@ class CBMPyAnnotationEditor(QWidget):
         )
         self.show()
 
+def rgb2hex(rgb):
+    return '#%02x%02x%02x' % rgb
 
 def writeLogBLAST(log, inp_time, input_fasta, metalink, metap):
     if not os.path.exists(log):
